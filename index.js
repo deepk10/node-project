bd54cfb4e6d3b0387409cc36322f76752c4fd29d
deepak.tyagi <deepaktyagidt90@gmail.com>
2023-01-15 23:19:38 +0530

    index update file and and app.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("deepak");
-console.log("test");
+const app = require("./app");
+//console.log(app.z());  // call variables & function from another file
+
+/******Array ****/
+const arr = [2, 4, 2, 3, 6, 8];
+let cnt = 0;
+var result = arr.filter((i) => {
+  return i === 3;
+});
+//console.log(result);
+/******Array end****/
