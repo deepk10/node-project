3d4cab373c5d0b55140fd74bd693b87b8867920b
deepak.tyagi <deepaktyagidt90@gmail.com>
2023-01-24 19:51:22 +0530

    update 24 jan1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer((req, resp) => {
-    resp.write("<h1>1hello</h1>");
+    resp.write("<h1>hello</h1>");
     resp.end();
   })
   .listen(1106); // createServer take function as a parameter
