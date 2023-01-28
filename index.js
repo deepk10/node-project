e4a51b8b4a86cdb7e6541d190f15848f9654575d
deepak.tyagi <deepaktyagidt90@gmail.com>
2023-01-28 21:02:29 +0530

    file update 28 jan2023 1

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-http = require("http");
-// this a arrow function
-const arrFnc = (req, resp) => {
-  resp.write("hello 123");
-  resp.end();
-};
-http.createServer(arrFnc).listen(1106);
+const color = require("color");
